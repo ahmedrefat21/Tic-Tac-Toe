76f6577f148cf8bb39f41fd2cf903315e7e472a0
sarrahAshrraf <sarahsoou01@gmail.com>
2024-01-01 19:28:41 +0200

    deleting the codes in controller files
    No need for them now

--- src/tic/tac/toe/PlayerNameController.java
+++ src/tic/tac/toe/PlayerNameController.java
@@ -26,48 +26,7 @@ import javafx.stage.Stage;
  * @author Kimo Store
  */
 public class PlayerNameController implements Initializable {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-    @FXML
-    TextField playerTwoTextField;
-      @FXML
-    TextField playerOneTextField;
-    @FXML
-    AnchorPane playerAnchor;
-    @FXML
-    AnchorPane gameAnchore;
-    @FXML
-    AnchorPane mainPageAnchor;
-    
 
-    @FXML
-    private void startGameButton(ActionEvent event) throws IOException {
-        String player1 = playerTwoTextField.getText();
-        String player2 = playerOneTextField.getText();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Game.fxml"));
-        root = loader.load();
-        GameController sc2 = loader.getController();
-        sc2.displayNames(player1,player2);
-        
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-        
-        gameAnchore = FXMLLoader.load(getClass().getResource("Game.fxml"));
-        playerAnchor.getChildren().setAll(gameAnchore);
-    
-    }
-    @FXML
-    private void backButton(ActionEvent event) throws IOException {
-    
-        mainPageAnchor = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-        playerAnchor.getChildren().setAll(mainPageAnchor);
-    
-    }
-    
-    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
