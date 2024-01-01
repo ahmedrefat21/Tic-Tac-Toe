0b2501c163d347744730553a6789015eb8aec6aa
sarrahAshrraf <sarahsoou01@gmail.com>
2024-01-01 18:37:21 +0200

    modify main page base img paths and deleting controller

--- src/tic/tac/toe/MainPageBase.java
+++ src/tic/tac/toe/MainPageBase.java
@@ -6,7 +6,6 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 
 public abstract class MainPageBase extends AnchorPane {
 
@@ -14,13 +13,10 @@ public abstract class MainPageBase extends AnchorPane {
     protected final ImageView aboutImage;
     protected final Rectangle computerRectangle;
     protected final ImageView imageView0;
-    protected final Text text;
     protected final Rectangle localRectangle;
     protected final Rectangle onlineRectangle;
     protected final ImageView imageView1;
     protected final ImageView imageView2;
-    protected final Text text0;
-    protected final Text text1;
     protected final Button localButton;
     protected final Button onlineButton;
     protected final Button compButton;
@@ -31,13 +27,10 @@ public abstract class MainPageBase extends AnchorPane {
         aboutImage = new ImageView();
         computerRectangle = new Rectangle();
         imageView0 = new ImageView();
-        text = new Text();
         localRectangle = new Rectangle();
         onlineRectangle = new Rectangle();
         imageView1 = new ImageView();
         imageView2 = new ImageView();
-        text0 = new Text();
-        text1 = new Text();
         localButton = new Button();
         onlineButton = new Button();
         compButton = new Button();
@@ -55,7 +48,7 @@ public abstract class MainPageBase extends AnchorPane {
         imageView.setFitWidth(788.0);
         imageView.setPickOnBounds(true);
         imageView.setPreserveRatio(true);
-        imageView.setImage(new Image(getClass().getResource("../../../assets/images/Splash%20Screen.png").toExternalForm()));
+        imageView.setImage(new Image(getClass().getResource("/assets/images/splashscreen.png").toExternalForm()));
 
         aboutImage.setFitHeight(104.0);
         aboutImage.setFitWidth(115.0);
@@ -63,7 +56,7 @@ public abstract class MainPageBase extends AnchorPane {
         aboutImage.setLayoutY(14.0);
         aboutImage.setPickOnBounds(true);
         aboutImage.setPreserveRatio(true);
-        aboutImage.setImage(new Image(getClass().getResource("../../../assets/images/about.png").toExternalForm()));
+        aboutImage.setImage(new Image(getClass().getResource("/assets/images/about.png").toExternalForm()));
 
         AnchorPane.setBottomAnchor(computerRectangle, 119.0);
         AnchorPane.setLeftAnchor(computerRectangle, 60.0);
@@ -87,19 +80,7 @@ public abstract class MainPageBase extends AnchorPane {
         imageView0.setLayoutY(205.0);
         imageView0.setPickOnBounds(true);
         imageView0.setPreserveRatio(true);
-        imageView0.setImage(new Image(getClass().getResource("../../../assets/images/computer.png").toExternalForm()));
-
-        AnchorPane.setBottomAnchor(text, 166.2548828125);
-        AnchorPane.setTopAnchor(text, 271.9384765625);
-        text.setFill(javafx.scene.paint.Color.valueOf("#f22853"));
-        text.setLayoutX(84.0);
-        text.setLayoutY(305.0);
-        text.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text.setStrokeWidth(0.0);
-        text.setText("Computer");
-        text.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
-        text.setWrappingWidth(142.6666899010561);
-        text.setFont(new Font("Comic Sans MS Bold", 30.0));
+        imageView0.setImage(new Image(getClass().getResource("/assets/images/computer.png").toExternalForm()));
 
         AnchorPane.setBottomAnchor(localRectangle, 119.0);
         AnchorPane.setLeftAnchor(localRectangle, 275.0);
@@ -110,7 +91,7 @@ public abstract class MainPageBase extends AnchorPane {
         localRectangle.setHeight(176.0);
         localRectangle.setLayoutX(275.0);
         localRectangle.setLayoutY(185.0);
-        localRectangle.setOnMouseClicked(this::handleButtonAction);
+
         localRectangle.setStroke(javafx.scene.paint.Color.TRANSPARENT);
         localRectangle.setStrokeType(javafx.scene.shape.StrokeType.INSIDE);
         localRectangle.setWidth(190.0);
@@ -137,7 +118,7 @@ public abstract class MainPageBase extends AnchorPane {
         imageView1.setLayoutY(205.0);
         imageView1.setPickOnBounds(true);
         imageView1.setPreserveRatio(true);
-        imageView1.setImage(new Image(getClass().getResource("../../../assets/images/local.png").toExternalForm()));
+        imageView1.setImage(new Image(getClass().getResource("/assets/images/local.png").toExternalForm()));
 
         AnchorPane.setBottomAnchor(imageView2, 185.0);
         AnchorPane.setLeftAnchor(imageView2, 537.0);
@@ -149,40 +130,16 @@ public abstract class MainPageBase extends AnchorPane {
         imageView2.setLayoutY(205.0);
         imageView2.setPickOnBounds(true);
         imageView2.setPreserveRatio(true);
-        imageView2.setImage(new Image(getClass().getResource("../../../assets/images/online-game.png").toExternalForm()));
+        imageView2.setImage(new Image(getClass().getResource("/assets/images/online-game.png").toExternalForm()));
 
-        AnchorPane.setBottomAnchor(text0, 165.2548828125);
-        AnchorPane.setTopAnchor(text0, 272.9384765625);
-        text0.setFill(javafx.scene.paint.Color.valueOf("#f22853"));
-        text0.setLayoutX(300.0);
-        text0.setLayoutY(317.0);
-        text0.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text0.setStrokeWidth(0.0);
-        text0.setText("Local");
-        text0.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
-        text0.setWrappingWidth(142.6666899010561);
-        text0.setFont(new Font("Comic Sans MS Bold", 30.0));
-
-        AnchorPane.setBottomAnchor(text1, 165.2548828125);
-        AnchorPane.setTopAnchor(text1, 272.9384765625);
-        text1.setFill(javafx.scene.paint.Color.valueOf("#f22853"));
-        text1.setLayoutX(513.0);
-        text1.setLayoutY(306.0);
-        text1.setStrokeType(javafx.scene.shape.StrokeType.OUTSIDE);
-        text1.setStrokeWidth(0.0);
-        text1.setText("Online");
-        text1.setTextAlignment(javafx.scene.text.TextAlignment.CENTER);
-        text1.setWrappingWidth(142.6666899010561);
-        text1.setFont(new Font("Comic Sans MS Bold", 30.0));
-
-        AnchorPane.setBottomAnchor(localButton, 132.0);
-        AnchorPane.setLeftAnchor(localButton, 286.0);
-        AnchorPane.setRightAnchor(localButton, 297.0);
-        AnchorPane.setTopAnchor(localButton, 293.0);
-        localButton.setLayoutX(286.0);
-        localButton.setLayoutY(293.0);
+        AnchorPane.setBottomAnchor(localButton, 127.0);
+        AnchorPane.setLeftAnchor(localButton, 293.0);
+        AnchorPane.setRightAnchor(localButton, 290.0);
+        AnchorPane.setTopAnchor(localButton, 298.0);
+        localButton.setLayoutX(293.0);
+        localButton.setLayoutY(298.0);
         localButton.setMnemonicParsing(false);
-        localButton.setOnAction(this::handleButtonAction);
+
         localButton.setPrefHeight(55.0);
         localButton.setPrefWidth(157.0);
         localButton.setStyle("-fx-background-radius: 40px;");
@@ -191,10 +148,10 @@ public abstract class MainPageBase extends AnchorPane {
         localButton.setTextFill(javafx.scene.paint.Color.valueOf("#f22853"));
         localButton.setFont(new Font("Comic Sans MS Bold", 30.0));
 
-        onlineButton.setLayoutX(513.0);
-        onlineButton.setLayoutY(294.0);
+        onlineButton.setLayoutX(514.0);
+        onlineButton.setLayoutY(296.0);
         onlineButton.setMnemonicParsing(false);
-        onlineButton.setOnAction(this::OnlineButtonAction);
+
         onlineButton.setPrefHeight(51.0);
         onlineButton.setPrefWidth(143.0);
         onlineButton.getStylesheets().add("/tic/tac/toe/loginfxml.css");
@@ -202,10 +159,10 @@ public abstract class MainPageBase extends AnchorPane {
         onlineButton.setTextFill(javafx.scene.paint.Color.valueOf("#f22853"));
         onlineButton.setFont(new Font("Comic Sans MS Bold", 28.0));
 
-        compButton.setLayoutX(77.0);
-        compButton.setLayoutY(297.0);
+        compButton.setLayoutX(82.0);
+        compButton.setLayoutY(298.0);
         compButton.setMnemonicParsing(false);
-        compButton.setOnAction(this::compButtonAction);
+       
         compButton.setPrefHeight(55.0);
         compButton.setPrefWidth(157.0);
         compButton.getStylesheets().add("/tic/tac/toe/loginfxml.css");
@@ -217,25 +174,15 @@ public abstract class MainPageBase extends AnchorPane {
         getChildren().add(aboutImage);
         getChildren().add(computerRectangle);
         getChildren().add(imageView0);
-        getChildren().add(text);
         getChildren().add(localRectangle);
         getChildren().add(onlineRectangle);
         getChildren().add(imageView1);
         getChildren().add(imageView2);
-        getChildren().add(text0);
-        getChildren().add(text1);
         getChildren().add(localButton);
         getChildren().add(onlineButton);
         getChildren().add(compButton);
 
     }
 
-    protected abstract void handleButtonAction(javafx.scene.input.MouseEvent mouseEvent);
-
-    protected abstract void handleButtonAction(javafx.event.ActionEvent actionEvent);
-
-    protected abstract void OnlineButtonAction(javafx.event.ActionEvent actionEvent);
-
-    protected abstract void compButtonAction(javafx.event.ActionEvent actionEvent);
 
 }
--- src/tic/tac/toe/MainPageController.java
+++ src/tic/tac/toe/MainPageController.java
@@ -23,39 +23,39 @@ import javafx.scene.shape.Rectangle;
  */
 public class MainPageController implements Initializable {
 
-    @FXML
-    private AnchorPane mainPageAnchor;
-   
-    @FXML
-    AnchorPane playerAnchor;
-    @FXML
-    AnchorPane levelAnchore;
-    @FXML
-    AnchorPane gameAnchore;
-    @FXML
-    AnchorPane signUpLogin;
-
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) throws IOException {
-      playerAnchor = FXMLLoader.load(getClass().getResource("PlayerName.fxml"));
-      mainPageAnchor.getChildren().setAll(playerAnchor);
-    }
-    
-        
-    @FXML
-    private void compButtonAction(ActionEvent event) throws IOException {
-     levelAnchore = FXMLLoader.load(getClass().getResource("LevelScreen.fxml"));      
-    mainPageAnchor.getChildren().setAll(levelAnchore); 
-      
-    }
-    @FXML
-      private void OnlineButtonAction(ActionEvent event) throws IOException { //nedd to be editied, can't find siguploginFXML
-     signUpLogin = FXMLLoader.load(getClass().getResource("signup_login_design.fxml"));      
-        mainPageAnchor.getChildren().setAll(signUpLogin); 
-      
-    }  
-    
+//    @FXML
+//    private AnchorPane mainPageAnchor;
+//   
+//    @FXML
+//    AnchorPane playerAnchor;
+//    @FXML
+//    AnchorPane levelAnchore;
+//    @FXML
+//    AnchorPane gameAnchore;
+//    @FXML
+//    AnchorPane signUpLogin;
+//
+//    
+//    @FXML
+//    private void handleButtonAction(ActionEvent event) throws IOException {
+//      playerAnchor = FXMLLoader.load(getClass().getResource("PlayerName.fxml"));
+//      mainPageAnchor.getChildren().setAll(playerAnchor);
+//    }
+//    
+//        
+//    @FXML
+//    private void compButtonAction(ActionEvent event) throws IOException {
+//     levelAnchore = FXMLLoader.load(getClass().getResource("LevelScreen.fxml"));      
+//    mainPageAnchor.getChildren().setAll(levelAnchore); 
+//      
+//    }
+//    @FXML
+//      private void OnlineButtonAction(ActionEvent event) throws IOException { //nedd to be editied, can't find siguploginFXML
+//     signUpLogin = FXMLLoader.load(getClass().getResource("signup_login_design.fxml"));      
+//        mainPageAnchor.getChildren().setAll(signUpLogin); 
+//      
+//    }  
+//    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
